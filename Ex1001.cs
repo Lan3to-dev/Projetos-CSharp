34ea272f6c04352c93340f6a573188b94fdba7a7
Lan3to <wellingtonlaneto@gmail.com>
2026-08-05 23:57:10 -0300

    Add files via upload

---
+++ Ex1001.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Ex1001
+{
+    static void Main()
+    {
+        int a, b, soma;
+        //Console.WriteLine("entre com dois valores");
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+        soma = a + b;
+        Console.WriteLine("X = " + soma);
+    }
+}
