ca8cb2011a19919093ddbc959a6fd9b44f4e6907
WellingtonLaneto <wellingtonlaneto@gmail.com>
2026-08-21 18:38:51 -0300

    .

---
+++ Ex5/Ex1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class AulaString
+{
+    static void Main()
+    {
+        string texto = "HELLO";
+        Console.WriteLine(texto[0]);
+        Console.WriteLine(texto[4]);
+        Console.WriteLine((int)texto[0]);
+    }
+}
---
+++ Ex5/Ex2.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Ex2
+{
+    static void Main()
+    {
+        string mensagem = "Olá, Mundo!";
+        Console.WriteLine(mensagem);
+        Console.WriteLine("Tamanho: " + mensagem.Length);
+    }
+}
