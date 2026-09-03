f7f4864269a928e8f6acf8fde2fa18d9cb45755d
Lan3to-dev <wellingtonlaneto@gmail.com>
2026-09-03 09:44:52 -0300

    aula03_09

---
+++ Ex9/Ex01.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Ex01
+{
+    static void Main()
+    {
+        int linhas = 3;
+        int colunas = 3;
+
+        int[,] matriz = new int[linhas, colunas];
+
+        Console.WriteLine("Digite os elementos da matriz:");
+
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.Write($"Elemento [{i},{j}]: ");
+                matriz[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("Matriz:");
+
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Ex9/Ex02.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Ex02
+{
+    static void lerMatriz(int[,] matriz)
+    {
+        int linhas = matriz.GetLength(0);
+        int colunas = matriz.GetLength(1);
+
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.Write($"Elemento [{i},{j}]: ");
+                matriz[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+
+    static void mostrarMatriz(int[,] matriz)
+    {
+        int linhas = matriz.GetLength(0);
+        int colunas = matriz.GetLength(1);
+
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.Write($"{matriz[i, j],3}| ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        int[,] matriz = new int[3, 3];
+
+        lerMatriz(matriz);
+
+        Console.WriteLine("Matriz:");
+
+        mostrarMatriz(matriz);
+    }
+}
